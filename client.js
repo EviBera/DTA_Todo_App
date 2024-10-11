bb962fa55ea9cea9bb0b82d5aee5d39ddcb4a7ce
EviBera <beraevi@gmail.com>
2024-10-11 14:43:58 +0200

    Implement create (post) todo feature

--- client.js
+++ client.js
@@ -1,11 +1,12 @@
 import promptSync from 'prompt-sync';
 
 const prompt = promptSync();
+const baseUrl = 'http://localhost:4000';
 
 const fetchAll = async () => {
     try {
-        const response = await fetch('http://localhost:4000');
-        const todos = await response.json();
+        const response = await fetch();
+        const todos = await response.json(baseUrl);
         console.log(todos);
     }
     catch (err) {
@@ -15,7 +16,7 @@ const fetchAll = async () => {
 
 const fetchById = async (id) => {
     try {
-        const url = 'http://localhost:4000/' + id;
+        const url = baseUrl + id;
         const response = await fetch(url);
         const todo = await response.json();
         console.log(todo);
@@ -25,14 +26,38 @@ const fetchById = async (id) => {
     }
 }
 
-function getIdFromUser () {
-    let id; 
-    while(isNaN(parseInt(id))){
+const fetchPost = async (task) => {
+    try {
+        const response = await fetch(baseUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({"task" : task})
+        });
+        const newTodo = await response.json();
+        console.log(`New todo: ${JSON.stringify(newTodo)} has been created successfully`);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+function getIdFromUser() {
+    let id;
+    while (isNaN(parseInt(id))) {
         id = prompt("Enter an id: ");
     }
     fetchById(id);
 }
 
+
+function getTaskFromUser() {
+    let task = prompt("If you would like to save a new task enter a short description: ");
+    fetchPost(task);
+}
+
+
 //fetchAll();
 //fetchById(3);
-getIdFromUser();
+//getIdFromUser();
+getTaskFromUser();
--- server.js
+++ server.js
@@ -4,6 +4,20 @@ import express from 'express';
 const port = 4000;
 const app = express();
 
+const counter = () => {
+    const todos = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+    const maxId = todos.reduce((acc, curr) => {
+        if(curr.id > acc.id){
+            return curr;
+        } else {
+            return acc;
+        }
+    }).id;
+
+    console.log(maxId);
+    return maxId;
+}
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -17,6 +31,24 @@ app.get('/:id', (req, res) => {
     const searchedTodo = todos.filter(t => t.id === parseInt(id))[0];
     res.json(searchedTodo);
 })
+
+app.post('/', (req, res) => {
+    let nextId = counter() + 1;
+    const newTodo = { 
+        id: nextId,
+        ...req.body,
+        isComplete: false
+    };
+    const todos = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
+    const extendedTodos = [...todos, newTodo];
+    try {
+        fs.writeFileSync('./data.json', JSON.stringify(extendedTodos, null, 2));
+        res.status(201).json(newTodo);
+    }
+    catch(err){
+        console.error(err);
+    }
+});
 
 app.listen(port, () => {
     console.log("Server listens on port: " + port);
